1334f56684bf0041cc00866b831448b89613454a
Bcalderon400 <briancalderon200@gmail.com>
2019-09-28 20:44:46 -0700

    created sequelize Burger modal, index.js

--- server.js
+++ server.js
@@ -1,34 +1,28 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
+// require models for syncing
+const db = require("./models");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+// setup express app
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
+// mount express middlewares
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
-app.use(express.static("public"));
+// setup handlebars for templating
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
+// import routes and give server access to them
+const routes = require("./controllers/burgersController.js");
+app.use(routes);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync().then(function() {
+// sync sequelize models then start server
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
-        console.log("App listening on PORT " + PORT);
+        console.log(`Server listening on PORT ${PORT}`);
     });
 });
