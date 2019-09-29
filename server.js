db395fa028cdfddb568436aca4d13c20594125eb
Bcalderon400 <briancalderon200@gmail.com>
2019-09-28 20:33:54 -0700

    deleted orm.js, connection.js is now json file for sequelize to use

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-// setup express app
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 8080;
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
 
-// mount express middlewares
-app.use(express.static("public"));
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// setup handlebars for templating
-const exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Static directory
+app.use(express.static("public"));
 
-// import routes and give server access to them
-const routes = require("./controllers/burgersController.js");
-app.use(routes);
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
 
-// start server
-app.listen(PORT, function() {
-  console.log(`Server listening on PORT ${PORT}`);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
 });
