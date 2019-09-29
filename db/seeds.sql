3e64e39b0cd32ca02d9ded7d821658c348cfb177
Bcalderon400 <briancalderon200@gmail.com>
2019-09-28 20:16:57 -0700

    duplicated logic from eat-da-burger repo for starting point

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Don't You Four Cheddar About Me");
+INSERT INTO burgers (burger_name) VALUES ("Gourdon Hamsey");
+INSERT INTO burgers (burger_name) VALUES ("Eggers Can't Be Cheesers");
